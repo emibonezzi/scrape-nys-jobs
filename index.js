09a626f0716361be029347babb2c5beffea427be
emibonezzi <emibonezzi@gmail.com>
2025-02-20 10:14:43 -0500

    Fix extraction logic

--- index.js
+++ index.js
@@ -10,32 +10,34 @@ exports.handler = async () => {
   await page.goto("https://statejobs.ny.gov/public/vacancyTable.cfm");
   // wait for table to load
   await page.waitForSelector("#vacancyTable");
-  // expand page length to X results
+  // expand page length to 100 results
   await page.select("select#dt-length-0", "100");
   let vacanciesIds = [];
   while (true) {
-    // Get the current "Next" button and retrieve its class list
-    const nextButtonClassList = await page.$eval("[data-dt-idx='next']", (el) =>
-      Array.from(el.classList)
-    );
-
-    // Extract vacancies id
+    // Extract page vacancies ids in array
     let pageVacancies = await page.$$eval(
       "#vacancyTable tr > td.dt-type-numeric",
       (rows) => rows.map((row) => row.textContent.trim())
     );
 
-    // Click the "Next" button (fetch fresh reference)
-    const nextButton = await page.$("[data-dt-idx='next']");
-
-    // push in general vacancies array
+    // push in global vacancies array
     vacanciesIds = [...vacanciesIds, ...pageVacancies];
 
-    if (nextButtonClassList.includes("disabled")) break; // Stop when disabled
+    // Get the current "Next" button and retrieve its class list
+    const nextButtonClassList = await page.$eval("[data-dt-idx='next']", (el) =>
+      Array.from(el.classList)
+    );
 
+    // get the current next button
+    const nextButton = await page.$("[data-dt-idx='next']");
+
+    // if button has "disabled" in his class list exit loop
+    if (nextButtonClassList.includes("disabled")) break;
+
+    // click next button
     await nextButton.click();
 
-    // Wait for table update
+    // wait for table update
     await page.waitForNetworkIdle();
   }
 
